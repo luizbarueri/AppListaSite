830271e6c01c0d498146b2efbb4a13b512593082
Luiz Amaro <Luizbarueri@gmail.com>
2020-01-08 23:26:47 -0300

    Adicionar arquivos de projeto.

---
+++ AppListaSite/AppListaSite/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace AppListaSite
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        List<string> sites = new List<string>
+        {
+            "db4free.net","youtube.com","globoplay.globo.com","w3schools.com","luizbarueri.com.br"
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+            ListaSites.ItemsSource = sites;
+        }
+
+        private void SeachSites_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var texto = SeachSites.Text;
+            ListaSites.ItemsSource = sites.Where(x => x.ToLower().Contains(texto));
+        }
+    }
+}
